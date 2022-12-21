260d7a4d0a2ef91561481d74b2694c235b57e5c0
Greidy Peña <greidykp@gmail.com>
2022-12-20 19:56:21 -0500

    Se agrega Soap Client

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use SoapClient;
 
 class Controller extends BaseController
 {
-    //
+    //Crea una instancia de soap client
+    public function createSoapClientInstance($wsdl)
+    {
+        return new SoapClient($wsdl, [
+            'stream_context' =>   stream_context_create([
+                'http' => [
+                    'user_agent' => 'PHPSoapClient'
+                ]
+            ]),
+            'cache_wsdl'     => WSDL_CACHE_NONE
+        ]);
+    }
 }
